15501d6e6f924704ca1fa6e44ea0c92b181ca77a
aino-nalanda <nalanda@ainosoft.com>
2016-05-05 16:51:34 +0530

    Added required code for starting scraping proxy setup

--- SeleniumPlacesScraper/src/main/java/com/ainosoft/seleniumplacescraper/manager/ProxyManager.java
+++ SeleniumPlacesScraper/src/main/java/com/ainosoft/seleniumplacescraper/manager/ProxyManager.java
@@ -2,37 +2,55 @@ package com.ainosoft.seleniumplacescraper.manager;
 
 import java.util.ArrayList;
 
+import com.ainosoft.seleniumplacescraper.dao.ProxyDetailsDao;
 import com.ainosoft.seleniumplacescraper.pojo.ProxyDetailsPojo;
-
-
+import com.ainosoft.seleniumplacescraper.scraper.ProxyScraper;
+import com.ainosoft.seleniumplacescraper.util.ScraperLogger;
 
 /**
  * @author nalanda@ainosoft.com
- *
  */
 public class ProxyManager implements Manager {
 
+	@SuppressWarnings("unused")
+	private static ScraperLogger scraperLogger = new ScraperLogger("ProxyManager");
+
+	ProxyDetailsDao proxyDao = new ProxyDetailsDao();
+	ProxyScraper proxyScraper = new ProxyScraper();
+
 	@Override
 	public void initializeAndStart(String url, String textToSearch) {
-/*		googleMapScraper = new GoogleMapScraper();
-		
-		googleMapScraper.setTextToSearch(textToSearch);
-		googleMapScraper.setUrl(url);
-		
-		ArrayList<PlacesDetailsPojo> placesDetailsPojoList = googleMapScraper.getScrapperDataList();
-		//Text to search
-		
-		//Site to search
-		//get proxy list from db
-		
-		//fetch data list frommscraper
-		
-		
-		//Save buld list to db	
-*/	}
-	
+		try {
+			proxyScraper.setUrl(url);
+
+			ArrayList<ProxyDetailsPojo> proxyPojoList = proxyScraper.startScrapingFetchProxyList();
+			if(proxyPojoList!=null){
+				if(!proxyPojoList.isEmpty()){
+					for(ProxyDetailsPojo proxyPojo : proxyPojoList){
+						proxyDao.saveProxyPojo(proxyPojo);
+					}
+				}
+			}
+		} catch (Exception e) {
+			ScraperLogger.log("ProxyManager :: initializeAndStart() ::",e); 
+		}
+	}
+
 	public ArrayList<ProxyDetailsPojo> getValidProxyList(){
+		try {
+			ArrayList<ProxyDetailsPojo> validProxyList = proxyDao.getValidProxyList();
+			return validProxyList;
+		} catch (Exception e) {
+			ScraperLogger.log("ProxyManager :: getValidProxyList() ::",e); 
+		}
 		return null;
-		
+	}
+	
+	public void updateProxyStatus(ArrayList<ProxyDetailsPojo> proxyPojoList){ 
+		try {
+			proxyDao.updateProxyStatus(proxyPojoList);
+		} catch (Exception e) {
+			ScraperLogger.log("ProxyManager :: updateProxyStatus() ::",e); 
+		}
 	}
 }
